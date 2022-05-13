ef948342d8e6e0a3b3cf6891fb349fd1b8a89378
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-05-13 23:39:10 +0100

    [Fix]: Issue

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -12,7 +12,7 @@ export const getPosts = async (req, res) => {
     }
 };
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
     const post = req.body;
 
     const newPost = new PostMessage(post);
