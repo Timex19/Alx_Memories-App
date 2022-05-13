dbdc31fe2abfced5793d823a44869a5c6d0e7099
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-05-14 00:13:31 +0100

    improvments

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -9,4 +9,14 @@ export const getPosts = () => async (dispatch) => {
     } catch (error) {
       console.log(error.message);
     }
-  };
+};
+
+export const createPosts = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: 'CREATE', payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
