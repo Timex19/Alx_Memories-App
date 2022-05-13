3001faeea9f63c7106fdcecd8d1641e6a8ef5bde
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-05-14 00:17:50 +0100

    [Update]: change the mongodb cluster name.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://Timi:Timex19@cluster0.qwcsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://Timi:Timex19@cluster0.qwcsl.mongodb.net/Alx_Memories_App?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
