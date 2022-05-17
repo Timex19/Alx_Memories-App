f5c5119bcbdee4b30aeb1d3016f6575e707b08e3
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-05-17 22:06:27 +0100

    Improvements

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -27,6 +27,26 @@ export const updatePost = (id, post) => async (dispatch) => {
 
     dispatch({ type: 'UPDATE', payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: 'DELETE', payload: id });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: 'UPDATE', payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+}
